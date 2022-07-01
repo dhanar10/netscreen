959ec749ca8c5f8821690704b4042e8dbcb120e4
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2022-07-02 05:49:11 +0700

    fix(nvenc): make it work with acceptable delay

--- netscreen.ps1
+++ netscreen.ps1
@@ -14,13 +14,13 @@ Start-Job -ScriptBlock {
 while (1) {
   Start-Sleep 2
 
-  ffmpeg -f gdigrab -framerate 60 -offset_x -1360 -video_size 1360x768 -show_region 0 -i desktop `
-    -c:v h264_amf -b:v 5M -usage 1 -profile:v 256 `
-    -fflags flush_packets -f mpegts udp://${IpAddress}:55555
-
   # ffmpeg -f gdigrab -framerate 60 -offset_x -1360 -video_size 1360x768 -show_region 0 -i desktop `
-  #    -c:v h264_nvenc -preset:v p1 -tune ull -zerolatency 1 -profile:v 1 -cbr 1 -b:v 5M -pix_fmt yuv420p `
-  #    -fflags flush_packets -f mpegts udp://${IpAddress}:55555
+  #   -c:v h264_amf -b:v 10M -usage 1 -profile:v 256 -quality 1 -rc cbr -frame_skipping 1 `
+  #   -fflags flush_packets -f mpegts udp://${IpAddress}:55555
+
+  ffmpeg -f gdigrab -framerate 60 -offset_x -1360 -video_size 1360x768 -show_region 0 -i desktop `
+      -c:v h264_nvenc -preset:v p1 -tune ull -zerolatency 1 -profile:v 0 -rc cbr -cbr 1 -b:v 20M -pix_fmt bgr0 `
+      -fflags flush_packets -f mpegts udp://${IpAddress}:55555
 
   #ffmpeg -f gdigrab -framerate 30 -offset_x -1360 -video_size 1360x768 -show_region 0 -i desktop `
   #    -c:v libx264 -preset:v ultrafast -tune zerolatency -b:v 5M `
